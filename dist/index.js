ace700025a9c27d6cf9d7c93c18c1d4a067dd5ca
arteemm <arteemmarteemm@gmail.com>
2024-04-01 20:22:00 +0300

    implement get

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const port = process.env.PORT || 3000;
+const videoItem = {
+    id: 0,
+    title: "string",
+    author: "string",
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: "2024-04-01T17:14:43.869Z",
+    publicationDate: "2024-04-01T17:14:43.869Z",
+    availableResolutions: ['P144'],
+};
+const videoItems = [videoItem];
+app.get('/videos', (req, res) => {
+    res.send(videoItems);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
