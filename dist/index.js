3e2625da4fe60e1b6bc50d4a475668df575a2175
arteemm <arteemmarteemm@gmail.com>
2024-04-03 18:58:29 +0300

    fix problem with versel

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const video_router_1 = require("./routes/video-router");
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const parserMiddleware = (0, body_parser_1.default)();
 app.use(parserMiddleware);
 app.use('/videos', video_router_1.videosRouter);
